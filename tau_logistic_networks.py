f8a9861c64ee72d0e0c12b60074516019aef3de4
Cheng <phynk_macheng@outlook.com>
2020-10-24 23:00:33 -0400

    tau_logistic_networks.py -- parallel different networks

--- tau_logistic_networks.py
+++ tau_logistic_networks.py
@@ -108,20 +108,6 @@ def eigenvalue_zero(x, A, fx, fxt, Degree, gx_i, gx_j):
     zeropoint = eigenvalue[np.argmin(np.abs(eigenvalue))]
     return np.array([np.real(zeropoint), np.imag(zeropoint)])
 
-def tau_try(initial_condition, A, fx, fxt, degree_weighted, gx_i, gx_j):
-    """TODO: Docstring for tau_try.
-
-    :arg1: TODO
-    :returns: TODO
-
-    """
-    tau_solution, nu_solution = fsolve(eigenvalue_zero, initial_condition, args=(A, fx, fxt, degree_weighted, gx_i, gx_j))
-    eigen_real, eigen_imag = eigenvalue_zero(np.array([tau_solution, nu_solution]), A, fx, fxt, degree_weighted, gx_i, gx_j)
-    if abs(eigen_real) < 1e-5 and abs(eigen_imag) < 1e-5:
-        return tau_solution 
-    else:
-        return 1
-
 def tau_eigenvalue(network_type, N, beta, nu_set, tau_set, arguments, seed, d=None):
     """TODO: Docstring for character_multi.
 
@@ -141,32 +127,18 @@ def tau_eigenvalue(network_type, N, beta, nu_set, tau_set, arguments, seed, d=No
     gx_i = xs/(D + E*xs + H*xs) - E * xs**2 / (D + E*xs + H*xs)**2
     gx_j = xs/(D + E*xs + H*xs) - H * xs**2 / (D + E*xs + H*xs)**2
 
-    tau_sol = np.ones((np.size(tau_set), np.size(nu_set))) * 10
-    p = mp.Pool(cpu_number)
-    tau_sol = p.starmap_async(tau_try, [(initial_condition, A, fx, fxt, degree_weighted, gx_i, gx_j) for initial_condition in np.array(np.meshgrid(tau_set, nu_set)).reshape(2, int(np.size(tau_set) * np.size(nu_set))).transpose()]).get()
-    p.close()
-    p.join()
-
+    tau_sol = []
+    for initial_condition in np.array(np.meshgrid(tau_set, nu_set)).reshape(2, int(np.size(tau_set) * np.size(nu_set))).transpose():
+        tau_solution, nu_solution = fsolve(eigenvalue_zero, initial_condition, args=(A, fx, fxt, degree_weighted, gx_i, gx_j))
+        eigen_real, eigen_imag = eigenvalue_zero(np.array([tau_solution, nu_solution]), A, fx, fxt, degree_weighted, gx_i, gx_j)
+        print(tau_solution)
+        if abs(eigen_real) < 1e-5 and abs(eigen_imag) < 1e-5:
+            tau_sol.append(tau_solution)
+        else:
+            tau_sol.append(1)
     tau_sol = np.array(tau_sol)
-    return tau_sol, A
-
-def tau_multi_critical(network_type, N, arguments, beta_set, seed, d=None, nu_set=None, tau_set=None, low=0.1, high=10):
-    """TODO: Docstring for tau_critical.
-
-    :arg1: TODO
-    :returns: TODO
-
-    """
-    tau_critical = np.zeros(np.size(beta_set))
-    for beta, i in zip(beta_set, range(np.size(beta_set))):
-        t1 = time.time()
-        tau_sol, A = tau_eigenvalue(network_type, N, beta, nu_set, tau_set, arguments, seed, d=d)
-        tau_sol = np.ravel(tau_sol)
-        tau_critical[i] = np.min(tau_sol[tau_sol>0])
-        index_critical = np.where(np.abs(tau_sol - tau_critical[i])<1e-10)[0]
-        t2 = time.time()
-        print(i, t2 - t1, tau_critical, index_critical)
-
+    tau_critical= np.min(tau_sol[tau_sol>0])
+    #index_critical = np.where(np.abs(tau_sol - tau_critical[i])<1e-10)[0]
     data = np.hstack((seed, tau_critical))
  
     column_name = [f'seed{i}' for i in range(len(seed))]
@@ -175,17 +147,107 @@ def tau_multi_critical(network_type, N, arguments, beta_set, seed, d=None, nu_se
     des = '../data/'
     if not os.path.exists(des):
         os.makedirs(des)
-    des_file = des + network_type + f'_N={N}_d=' + str(d).replace('.', '') + '_logistic.csv'
+    des_file = des + network_type + f'_N={N}_d=' + str(d) + '_logistic.csv'
     if not os.path.exists(des_file):
         df = pd.DataFrame(data.reshape(1, np.size(data)), columns = column_name)
         df.to_csv(des_file, index=None, mode='a')
     else:
         df = pd.DataFrame(data.reshape(1, np.size(data)))
         df.to_csv(des_file, index=None, header=None, mode='a')
-    return tau_critical
+
+    return None
+
+def tau_multi_critical(network_type, N, arguments, beta_set, seed_list, d=None, nu_set=None, tau_set=None, low=0.1, high=10):
+    """TODO: Docstring for tau_critical.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    for beta, i in zip(beta_set, range(np.size(beta_set))):
+        t1 = time.time()
+        p = mp.Pool(cpu_number)
+        p.starmap_async(tau_eigenvalue, [(network_type, N, beta, nu_set, tau_set, arguments, seed, d) for seed in seed_list]).get()
+        p.close()
+        p.join()
+
+        t2 = time.time()
+
+    return None
+
+def tau_evolution(network_type, N, beta, seed, arguments, delay1, delay2, criteria_delay, criteria_dyn, d):
+    """TODO: Docstring for tau_evolution.
+
+    :network_type: TODO
+    :N: TODO
+    :beta: TODO
+    :seed: TODO
+    :d: TODO
+    :returns: TODO
+
+    """
+    A, A_interaction, index_i, index_j, cum_index = network_generate(network_type, N, beta, seed, d)
+    N = np.size(A, 0)
+    initial_condition = np.ones(N) * 5 
+    t = np.arange(0, 1000,0.01)
+    dyn_dif = 1
+    delta_delay = delay2 - delay1
+    result = dict()
+    while delta_delay > criteria_delay:
+        if delay1 not in result:
+            dyn_all1 = ddeint_Cheng(mutual_multi_delay, initial_condition, t, *(delay1, 0, 0, N, index_i, index_j, A_interaction, cum_index, arguments))[-2000:]
+            diff1 = np.max(np.abs(dyn_all1 - np.mean(dyn_all1, 0)))
+            result[delay1] = diff1
+        if delay2 not in result:
+            dyn_all2 = ddeint_Cheng(mutual_multi_delay, initial_condition, t, *(delay2, 0, 0, N, index_i, index_j, A_interaction, cum_index, arguments))[-2000:]
+            diff2 = np.max(np.abs(dyn_all2 - np.mean(dyn_all2, 0)))
+            result[delay2] = diff2
+        if result[delay1] < criteria_dyn and (result[delay2] > criteria_dyn or np.isnan(result[delay2])):
+            delay1 = np.round(delay1 + delta_delay/2, 10)
+        elif result[delay1] > criteria_dyn or np.isnan(result[delay1]):
+            delay2 = np.round(delay1, 10)
+            delay1 = np.round(delay1 - delta_delay, 10)
+        delta_delay = delay2 - delay1 
+    print(seed, delay1, delay2)
+    data = np.hstack((seed, delay1))
+ 
+    column_name = [f'seed{i}' for i in range(len(seed))]
+    column_name.extend([ str(beta) for beta in beta_set])
+
+    des = '../data/'
+    if not os.path.exists(des):
+        os.makedirs(des)
+    des_file = des + network_type + f'_N={N}_d=' + str(d) + '_evolution.csv'
+    if not os.path.exists(des_file):
+        df = pd.DataFrame(data.reshape(1, np.size(data)), columns = column_name)
+        df.to_csv(des_file, index=None, mode='a')
+    else:
+        df = pd.DataFrame(data.reshape(1, np.size(data)))
+        df.to_csv(des_file, index=None, header=None, mode='a')
+
+    return delay2
+
+def tau_evolution_parallel(network_type, N, beta, seed_list, arguments, delay1, delay2, criteria_delay, criteria_dyn, d):
+    """TODO: Docstring for tau_evolution_parallel.
+
+    :network_type: TODO
+    :N: TODO
+    :beta: TODO
+    :: TODO
+    :returns: TODO
+
+    """
+
+    p = mp.Pool(cpu_number)
+    p.starmap_async(tau_evolution, [(network_type, N, beta, seed, arguments, delay1, delay2, criteria_delay, criteria_dyn, d) for seed in seed_list]).get()
+    p.close()
+    p.join()
+
+    return None
+
 
 imag = 1j
-N = 2500
+N = 1000
 
 tau_set = np.array([0.316])
 nu_set = np.array([5.2])
@@ -208,20 +270,19 @@ network_type = 'ER'
 network_type = 'SF'
 seed_set = np.arange(0, 500, 1).tolist()
 beta_set = np.arange(1, 2, 1)
-N_list = [1000]
+N_list = [50]
 
 kmin = [3]
 gamma = [3]
 d_list = np.hstack((np.meshgrid(kmin, gamma)[0], np.meshgrid(kmin, gamma)[1]))
 seed1 = [0]
-seed2 = np.arange(100).tolist()
+seed2 = np.arange(10).tolist()
 seed_list = np.hstack((np.meshgrid(seed1, seed2)[0], np.meshgrid(seed1, seed2)[1])).tolist()
 
 t1 = time.time()
 for N in N_list:
     for d in d_list:
-        for seed in seed_list:
-            tau_c = tau_multi_critical(network_type, N, arguments, beta_set, seed, nu_set = nu_set, tau_set = tau_set, d = d)
+        tau_multi_critical(network_type, N, arguments, beta_set, seed_list, nu_set = nu_set, tau_set = tau_set, d = d)
 t2 = time.time()
 print(t2 -t1)
 
@@ -230,4 +291,18 @@ d1 = 0.31
 d2 = 0
 d3 = 0
 seed = 499
-#dyn_all = evolution(network_type, N, beta, seed, arguments, d1, d2, d3, d)
+
+seed = [0, 5]
+delay1 = 0.2
+delay2 = 0.4
+criteria_delay = 1e-3
+criteria_dyn = 1e-6
+d = [3, 3]
+'''
+t1 = time.time()
+tau = tau_evolution_parallel(network_type, N, beta, seed_list, arguments, delay1, delay2, criteria_delay, criteria_dyn, d)
+t2 = time.time()
+print(t2 - t1)
+'''
+
+#dyn_all = evolution(network_type, N, beta, seed, arguments, 0.24, 0, 0, d)
